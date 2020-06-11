0d890ad73d08ad4b89c45939b0ad4b6bf74cabfa
RedCat42 <37554712+RedCat42@users.noreply.github.com>
2020-06-11 19:59:50 +0200

    Alternative à Program.cs
    Ce programme est là pour demander une note.

---
+++ Program_Ask.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace AERGO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (!File.Exists(@"../network_config.ini")) // Si network_config.ini n'existe pas
+            {
+                Console.WriteLine("Erreur : quelqu'un a mal fait son travail (moi)");
+                Environment.Exit(0);
+            } else // S'il existe
+            {
+                Reseau network = Reseau.Load(); // On le charge
+                // network.Save(); Console.Read();
+
+                network.Feed(new double[] {
+                Convert.ToDouble(args[0]) / 255.0,
+                Convert.ToDouble(args[1]) / 255.0,
+                Convert.ToDouble(args[2]) / 255.0
+                });
+                double[] guess = network.output.ToArrayVector();
+
+                network.input.Debug("Entrée");
+                network.output.Debug("Sortie");
+
+                Console.WriteLine(guess[0] * 1.0); // On donne la note devinée
+            }
+        }
+    }
+}
